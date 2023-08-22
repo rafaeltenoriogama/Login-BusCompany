6c673392efb93e1ca567aab43f276ce5aeaef3a7
Rafael Mercer <rafael_tenoriox2@hotmail.com>
2023-08-22 11:53:32 -0300

    Exemplo de tabela em MySQL/MariaDB utilizado na criação de usuários para login.

--- users_table.sql
+++ users_table.sql
@@ -10,3 +10,6 @@ VALUES
   ('ademir', 'abc123', 'Ademir Pereira', 'ademir@gmail.com'),
   ('sabo', 'sabo123', 'Marco Sabo', 'sabo@gmail.com'),
   ('rafa', 'rafa123', 'Rafael Mercer', 'rafael@gmail.com');
+/* Nomes genéricos, e-mail genéricos. 
+O ideal é substituir por sua tabela de usuários já com todos os seus critérios definidos. 
+Como: E-mail, Senha, 2FA, etc. */ 
